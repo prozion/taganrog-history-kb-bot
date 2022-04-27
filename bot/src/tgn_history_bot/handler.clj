75db3241ecdc9fa90f93f1821d8d21b4d4f91fcb
Denis Shirshov <denis.shirshov@gmail.com>
2022-04-27 14:50:41 +0300

    b-2.9.3

--- bot/src/tgn_history_bot/handler.clj
+++ bot/src/tgn_history_bot/handler.clj
@@ -22,7 +22,7 @@
       ;               (tb/send-text body chat-id))
       "list_modern_streets" (tb/send-text (kb/get-modern-streets) chat-id)
       "init_kb" (do
-                  (sparql/init-db "../../factbase/houses/blocks.tree")
+                  (sparql/init-db "../factbase/houses/blocks.tree")
                   (tb/send-text "База знаний проинициализована." chat-id))
       "which_block" (let [ans (or
                                 (some-> text tb/get-command-body city/normalize-address sparql/find-blocks first)
