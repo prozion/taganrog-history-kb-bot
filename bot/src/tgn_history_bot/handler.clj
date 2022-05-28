2217a4a40fc0deacbc6c79bccf160d96ae54393f
Denis Shirshov <denis.shirshov@gmail.com>
2022-05-28 17:58:51 +0300

    minor changes

--- bot/src/tgn_history_bot/handler.clj
+++ bot/src/tgn_history_bot/handler.clj
@@ -48,8 +48,8 @@
                                     (format "Есть данные про дома:\n%s"
                                       (->> sparql-result (map :house) (map name) (sort city/compare-address) (map city/get-canonical-address) (s/join "\n"))))
                   ]
-              (--- text-result))
-              ; (tb/send-text text-result chat-id :html))
+              ; (--- text-result))
+              (tb/send-text text-result chat-id :html))
       (do
         (println (format "Couldn't process a line: '%s'" text)))
       )))
